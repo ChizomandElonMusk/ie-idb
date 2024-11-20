f46f6d2fdbdeabb851d0263ff8afce1c529af34b
Chizom <chizomreal@gmail.com>
2024-11-20 21:34:55 +0100

    minor update

---
+++ .nuxt/dist/server/pages/about.js
@@ -0,0 +1,160 @@
+exports.ids = [9,5];
+exports.modules = {
+
+/***/ 37:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/logo.e97530d.png";
+
+/***/ }),
+
+/***/ 40:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Logo.vue?vue&type=template&id=ac56e43a
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', [_vm._ssrNode("<div class=\"row\" style=\"margin-top: 0px\"><div class=\"col s12 m3\"></div> <div class=\"col s12 m6 center\"><img" + _vm._ssrAttr("src", __webpack_require__(37)) + " alt class=\"responsive-img circle\" style=\"max-width:100px; max-height:100px\"></div> <div class=\"col s12 m3\"></div></div>")]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/Logo.vue?vue&type=template&id=ac56e43a
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Logo.vue?vue&type=script&lang=js
+/* harmony default export */ var Logovue_type_script_lang_js = ({
+  name: 'TinaLogo'
+});
+// CONCATENATED MODULE: ./components/Logo.vue?vue&type=script&lang=js
+ /* harmony default export */ var components_Logovue_type_script_lang_js = (Logovue_type_script_lang_js); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/Logo.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_Logovue_type_script_lang_js,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "73e52696"
+  
+)
+
+/* harmony default export */ var Logo = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 41:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/home.adfced5.svg";
+
+/***/ }),
+
+/***/ 75:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/about.vue?vue&type=template&id=f811112e&scoped=true
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticStyle: {
+      "padding-left": "40px",
+      "padding-right": "40px",
+      "padding-top": "20px"
+    }
+  }, [_vm._ssrNode("<div class=\"row\" data-v-f811112e>", "</div>", [_c('Logo', {
+    staticStyle: {
+      "margin-bottom": "30px",
+      "margin-top": "100px"
+    }
+  })], 1), _vm._ssrNode(" "), _vm._ssrNode("<div class=\"row\" data-v-f811112e>", "</div>", [_vm._ssrNode("<div class=\"row\" data-v-f811112e><div class=\"col s12 center\" data-v-f811112e><b class=\"red-text\" data-v-f811112e>IDB</b></div></div> <br data-v-f811112e> <div class=\"row\" data-v-f811112e><div class=\"col s12 center\" data-v-f811112e><b class=\"grey-text\" data-v-f811112e>Version 1.0.0</b></div></div> <div class=\"row\" data-v-f811112e><div class=\"col s12 center\" data-v-f811112e><b class=\"grey-text\" data-v-f811112e>Copyright Ikeja Electric, <br data-v-f811112e> All rights Reserved</b></div></div> "), _vm._ssrNode("<div class=\"row\" style=\"margin-top: 40px\" data-v-f811112e>", "</div>", [_vm._ssrNode("<div class=\"col s12 center\" data-v-f811112e>", "</div>", [_c('nuxt-link', {
+    staticStyle: {
+      "border-radius": "10px"
+    },
+    attrs: {
+      "to": "./settings"
+    }
+  }, [_c('img', {
+    staticClass: "responsive-img",
+    staticStyle: {
+      "max-width": "50px",
+      "filter": "hue-rotate(180deg)"
+    },
+    attrs: {
+      "src": __webpack_require__(41),
+      "alt": ""
+    }
+  })])], 1)])], 2)], 2);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/about.vue?vue&type=template&id=f811112e&scoped=true
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/about.vue?vue&type=script&lang=js
+/* harmony default export */ var aboutvue_type_script_lang_js = ({
+  layout: 'admin_main',
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {}
+});
+// CONCATENATED MODULE: ./pages/about.vue?vue&type=script&lang=js
+ /* harmony default export */ var pages_aboutvue_type_script_lang_js = (aboutvue_type_script_lang_js); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/about.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_aboutvue_type_script_lang_js,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "f811112e",
+  "4f8cb4f4"
+  
+)
+
+/* harmony default export */ var about = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {Logo: __webpack_require__(40).default})
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=about.js.map
